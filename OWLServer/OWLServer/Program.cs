036ab81be4613fbdcb044929e43f65534908f16a
marxDa <marxd262@gmail.com>
2025-05-26 17:35:50 +0200

    Audioservice as Singilton

--- OWLServer/OWLServer/Program.cs
+++ OWLServer/OWLServer/Program.cs
@@ -10,6 +10,7 @@ builder.Services.AddRazorComponents()
 
 builder.Services.AddSingleton<GameStateService>();
 builder.Services.AddSingleton<ExternalTriggerService>();
+builder.Services.AddSingleton<AudioService>();
 builder.Services.AddControllers();
 
 builder.Services.AddRadzenComponents();
