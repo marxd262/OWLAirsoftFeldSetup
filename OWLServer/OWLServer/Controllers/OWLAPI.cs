57e9c870b89415a38e2fbbf598e73467a3239c23
marxDa <marxd262@gmail.com>
2025-07-06 15:31:38 +0200

    Apiaufrufe sind jetzt Asyncron

--- OWLServer/OWLServer/Controllers/OWLAPI.cs
+++ OWLServer/OWLServer/Controllers/OWLAPI.cs
@@ -36,28 +36,28 @@ namespace OWLServer.Controllers
         [HttpPost("RegisterTower")]
         public ActionResult RegisterTower(string id, string ip)
         {
-            GameStateService.TowerManagerService.RegisterTower(id, ip);
+            Task.Run(() => GameStateService.TowerManagerService.RegisterTower(id, ip));
             return Ok();
         }
 
         [HttpPost("CaptureTower")]
         public ActionResult CaptureTower(string id, TeamColor color)
         {
-            GameStateService.TowerManagerService.TowerChangeColor(id, color);
+            Task.Run(() => GameStateService.TowerManagerService.TowerChangeColor(id, color));
             return Ok();
         }
 
         [HttpPost("TowerButtonPressed")]
         public ActionResult TowerButtonPressed(string id, TeamColor color)
         {
-            GameStateService.TowerManagerService.HandleTowerButtonPressed(id, color);
+            Task.Run(() => GameStateService.TowerManagerService.HandleTowerButtonPressed(id, color));
             return Ok();
         }
         
         [HttpPost("TowerButtonReleased")]
         public ActionResult TowerButtonReleased(string id)
         {
-            GameStateService.TowerManagerService.HandleTowerButtonReleased(id);
+            Task.Run(() => GameStateService.TowerManagerService.HandleTowerButtonReleased(id));
             return Ok();
         }
     }
