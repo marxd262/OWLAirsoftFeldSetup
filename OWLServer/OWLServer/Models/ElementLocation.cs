821eb7d7dd024f1a5f13b47fd9f4f12c441538fc
marxDa <marxd262@gmail.com>
2025-08-23 00:38:18 +0200

    Change Map und Berechnung der Clickposition verwendet jetzt einen Offset

--- OWLServer/OWLServer/Models/ElementLocation.cs
+++ OWLServer/OWLServer/Models/ElementLocation.cs
@@ -4,14 +4,17 @@
     {
         public double Width { get; set; }
         public double Height { get; set; }
+        
+        public int offsetTop { get; set; }
+        public int offsetLeft { get; set; }
         public double Top { get; set; }
         public double Left { get; set; }
 
         public string ToLocationString()
         {
             return $"position:absolute;" +
-                   $"top:calc(100% * {Top.ToString().Replace(',', '.')});" +
-                   $"left:calc(100% * {Left.ToString().Replace(',', '.')});";
+                   $"top:calc(100% * {Top.ToString().Replace(',', '.')} - 1.5rem);" +
+                   $"left:calc(100% * {Left.ToString().Replace(',', '.')} - 1.5rem);";
         }
     }
 }
