d808208eed51c47bf6cabd818a8dbe91c581ca17
marxDa <marxd262@proton.me>
2026-05-01 16:16:30 +0200

    fix: forward map click events through MapCanvas

--- OWLServer/OWLServer/Components/MapComponents/MapCanvas.razor.cs
+++ OWLServer/OWLServer/Components/MapComponents/MapCanvas.razor.cs
@@ -1,8 +1,10 @@
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
 
 namespace OWLServer.Components.MapComponents;
 
 public partial class MapCanvas : ComponentBase
 {
     [Parameter] public RenderFragment? ChildContent { get; set; }
+    [Parameter] public EventCallback<MouseEventArgs> OnMapClick { get; set; }
 }
